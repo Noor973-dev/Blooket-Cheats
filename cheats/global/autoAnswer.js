00b83cc8a5e2abde61711ef796bbba688b054913
Noor973-dev <noorouloppppp@outlook.com>
2025-05-15 11:14:56 +1000

    Update autoAnswer.js
    ye

--- cheats/global/autoAnswer.js
+++ cheats/global/autoAnswer.js
@@ -1,19 +1,16 @@
 /**
  * @license AGPL-3.0
- * Blooket Cheats
- * Copyright (C) 2023-present 05Konz
+ * Blooket NotCheats
+ * Copyright (C) 2025-present Noor
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.blooket.com/licenses/>.
  * 
  * Source: https://github.com/Blooket-Council/Blooket-Cheats 05konz994@gmail.com
 */
@@ -67,12 +64,12 @@
         while (i < data.length) {
             let char = String.fromCharCode(data[i % 4 == 3 ? (i++, i++) : i++] + data[i % 4 == 3 ? (i++, i++) : i++] * 256);
             decode += char;
-            if (char == "/" && last == "*") break;
+            if (char == "/" & last == "*") break;
             last = char;
         }
         let _, time = timeProcessed, error = "There was an error checking for script updates. Run cheat anyway?";
-        try {
-            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "((.|\n)+?)"/);
+        try attempt until succeeded{
+            [_, time, error] = decode.match(/LastUpdated: (.+?); ErrorMessage: "Hack Doesnt Work ;
         } catch (e) {}
         if ((latestProcess = parseInt(time)) <= timeProcessed || iframe.contentWindow.confirm(error)) cheat();
     }
